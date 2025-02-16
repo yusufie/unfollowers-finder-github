f2e605cc7377ee7f8310fd2940e836ebc0b5eee8
Frontend Developer <90094867+yusufie@users.noreply.github.com>
2025-02-16 18:04:07 +0300

    fix: update username validation to require a minimum of 1 character

--- src/components/Forms/InputForm.tsx
+++ src/components/Forms/InputForm.tsx
@@ -13,8 +13,8 @@ import {
 import { Input } from "@/components/ui/input"
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  username: z.string().min(1, {
+    message: "Username must be at least 1 characters.",
   }),
 })
 

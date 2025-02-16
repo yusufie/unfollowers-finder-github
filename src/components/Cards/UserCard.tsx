b9b82352f87e666d9eb9f2a86931e3f0c441e619
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-02-16 11:31:52 +0300

    fix: add optional chaining to "unfollower" props.

--- src/components/Cards/UserCard.tsx
+++ src/components/Cards/UserCard.tsx
@@ -15,22 +15,22 @@ type CardProps = React.ComponentProps<typeof Card> & {
 
 export default function CardDemo({ className, unfollower, ...props }: CardProps) {
   return (
-    <Link href={`${unfollower.html_url}`} target="_blank">
+    <Link href={`${unfollower?.html_url}`} target="_blank">
     <Card 
       className={cn("flex flex-row items-center w-96 gap-8 p-4", className)} {...props}
       >
           <Image
-            src={unfollower.avatar_url}
-            alt={unfollower.login} width={48} height={48}
+            src={unfollower?.avatar_url}
+            alt={unfollower?.login} width={48} height={48}
             className="rounded-full"
            />
           
           <div className="flex-1 space-y-1">
             <p className="text-sm font-medium leading-none">
-            {unfollower.login}
+            {unfollower?.login}
             </p>
             <p className="text-sm text-muted-foreground">
-              {unfollower.html_url}
+              {unfollower?.html_url}
             </p>
           </div>
 
